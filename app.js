e1528c4fae6772a583cac8aff4c70874afa3224c
konstanen <konsta.raitanen2@gmail.com>
2021-02-24 22:00:28 +0200

    'testissa'

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const cors = require('cors');
 const exphbs = require('express-handlebars');
-const posts = require('./Posts');
+
 
 require('dotenv/config');
 
